3d061c4b16562411a1a914df665ac008c02d211d
Oleg Adamov <olegadamov84@gmail.com>
2022-09-08 13:42:49 +0300

    settings

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
 import axios from './axios'
+
+// Module with Babel tranformation
+// import SimpleLightbox from "simplelightbox";
+
+// Plain ES Module without Babel
+// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
