97398d1bc7f08698eedb683e7d12d33ce14785a2
Oleg Adamov <olegadamov84@gmail.com>
2022-09-09 22:55:59 +0300

    start file

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-// import axios from './axios'
-
-// Module with Babel tranformation
-// import SimpleLightbox from "simplelightbox";
-
-// Plain ES Module without Babel
-// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
--- src/js/example.js
+++ src/js/example.js
@@ -1,26 +1,24 @@
-
 // Описан в документации
-// import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox";
 // Дополнительный импорт стилей
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from 'notiflix';
 
-// import axios from './axios'
-
-// Module with Babel tranformation
-// import SimpleLightbox from "simplelightbox";
-
-// Plain ES Module without Babel
-// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+const DEBOUNCE_DELAY = 300;
 
 
 
 
+const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
 
-// const { height: cardHeight } = document
-//   .querySelector(".gallery")
-//   .firstElementChild.getBoundingClientRect();
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
 
-// window.scrollBy({
-//   top: cardHeight * 2,
-//   behavior: "smooth",
-// });
+
+// Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.")
+//  Notiflix.Notify.failure("")
+
